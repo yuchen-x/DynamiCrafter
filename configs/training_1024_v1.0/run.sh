443987872c8b9b1554c417a8ba5802468590b2a7
xiaomotor <yuchenx424@gmail.com>
2024-07-10 20:42:29 +0800

    successfully run on server

--- configs/training_1024_v1.0/run.sh
+++ configs/training_1024_v1.0/run.sh
@@ -10,19 +10,19 @@ name="training_1024_v1.0"
 config_file=configs/${name}/config.yaml
 
 # save root dir for logs, checkpoints, tensorboard record, etc.
-save_root="<YOUR_SAVE_ROOT_DIR>"
+save_root="~/autodl-tmp/logs/dynamicrafter"
 
 mkdir -p $save_root/$name
 
 ## run
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch \
---nproc_per_node=$HOST_GPU_NUM --nnodes=1 --master_addr=127.0.0.1 --master_port=12352 --node_rank=0 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch \
+--nproc_per_node=4 --nnodes=1 --master_addr=127.0.0.1 --master_port=12352 --node_rank=0 \
 ./main/trainer.py \
 --base $config_file \
 --train \
 --name $name \
 --logdir $save_root \
---devices $HOST_GPU_NUM \
+--devices 4 \
 lightning.trainer.num_nodes=1
 
 ## debugging
